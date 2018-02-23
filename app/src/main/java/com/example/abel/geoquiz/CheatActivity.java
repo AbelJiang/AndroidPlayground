687160352b2c3e139a0a80b28c2260aa65692fc8
Wen Jiang <abeljiang@hotmail.com>
2018-02-22 22:05:01 -0800

    Finish GeoQuiz

--- app/src/main/java/com/example/abel/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/abel/geoquiz/CheatActivity.java
@@ -1,9 +1,14 @@
 package com.example.abel.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -53,6 +58,22 @@ public class CheatActivity extends AppCompatActivity {
             }
             isAnswerShown=true;
             setAnswerShownResult(isAnswerShown);
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                int cx=mShowAnswer.getWidth()/2;
+                int cy=mShowAnswer.getHeight()/2;
+                float radius=mShowAnswer.getWidth();
+                Animator anim= ViewAnimationUtils.createCircularReveal(mShowAnswer,cx,cy,radius,0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        super.onAnimationCancel(animation);
+                        mShowAnswer.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
+            }else {
+                mShowAnswer.setVisibility(View.VISIBLE);
+            }
         });
     }
 
